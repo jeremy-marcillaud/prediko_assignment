e8a7687172eb868f27d47b130adfec347e25d2b0
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-02 12:54:11 +0100

    uncomment spinner on loading page

--- pages/admin/users/index.tsx
+++ pages/admin/users/index.tsx
@@ -9,7 +9,11 @@ export default function Page() {
   const [searchTerm, setSearchTerm] = useState("");
 
   if (!users && !isError) {
-    return isLoading;
+    return (
+      <div className=" h-screen flex justify-center items-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
 
   const handleSearch = (e: any) => {
