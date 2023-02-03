cfc02fdbef62919ddc8a61df0f8498d35dd333eb
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-03 11:15:35 +0100

    circlebutton on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,21 @@ import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { MdAddBox, MdArrowForward } from "react-icons/md";
+import CircleButton from "../components/atoms/circleButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="bg-slate-100 h-screen w-screen flex items-center justify-center">
-      <Link
-        href={`/admin/users`}
-        className="mb-5 w-96 h-20 bg-white border rounded-lg shadow-lg shadow-zinc-300 flex justify-around items-center"
-      >
+      <div className="mb-5 w-96 h-20 bg-white border rounded-lg shadow-lg shadow-zinc-300 flex justify-around items-center">
         <div>
           <p>Dashboard</p>
         </div>
-        <div className=" bg-yellow-600 rounded-full h-5 w-5 flex items-center justify-center hover:bg-yellow-500">
-          <div>
-            <MdArrowForward className="text-white" />
-          </div>
-        </div>
-      </Link>
+        <CircleButton path="/admin/users">
+          <MdArrowForward className="text-white" />
+        </CircleButton>
+      </div>
     </div>
   );
 }
