394aedbea9d72af508893abc74af10532667dbf0
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-07 21:26:43 +0100

    typescript error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,28 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import MenuLayout from "../components/menuLayout";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <>
-      <MenuLayout>
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
+      {Component.authPage ? (
         <Component {...pageProps} />
-      </MenuLayout>
-      )
+      ) : (
+        <MenuLayout>
+          <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          <Component {...pageProps} />
+        </MenuLayout>
+      )}
     </>
   );
 }
