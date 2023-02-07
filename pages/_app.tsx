3cc7a6ec3ba76d46ba2fa637447e960f13326f77
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-07 17:41:51 +0100

    try to fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,24 +6,21 @@ import { ToastContainer } from "react-toastify";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {Component.authPage ? (
+      <MenuLayout>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <Component {...pageProps} />
-      ) : (
-        <MenuLayout>
-          <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            hideProgressBar={true}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <Component {...pageProps} />
-        </MenuLayout>
-      )}
+      </MenuLayout>
+      )
     </>
   );
 }
