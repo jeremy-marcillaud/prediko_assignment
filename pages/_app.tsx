35269d3102b5792bc91f8076abb358d223789a62
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-07 17:53:16 +0100

    test toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import MenuLayout from "../components/menuLayout";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
