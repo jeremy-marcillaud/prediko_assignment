764a930d5fbc7e6e6df24e85fee823397df8c932
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-07 21:30:15 +0100

    test prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,24 @@ import { ToastContainer } from "react-toastify";
 export default function App({ Component, pageProps }: any) {
   return (
     <>
-      {Component.authPage ? (
+      {/* {Component.authPage ? (
         <Component {...pageProps} />
-      ) : (
-        <MenuLayout>
-          <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            hideProgressBar={true}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <Component {...pageProps} />
-        </MenuLayout>
-      )}
+      ) : ( */}
+      <MenuLayout>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <Component {...pageProps} />
+      </MenuLayout>
+      {/* )} */}
     </>
   );
 }
