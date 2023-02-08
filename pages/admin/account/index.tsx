8b2bb1968a4e52b0f20e353f519455ad34092916
Jérémy Marcillaud <jeremy@syncly.fr>
2023-02-08 14:06:16 +0100

    account page

---
+++ pages/admin/account/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import NavDashboard from "../../../components/molecules/navDashboard";
+
+export default function Page() {
+  let [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div className="p-5">
+      <NavDashboard setOpen={setOpen} open={open} variant="save">
+        Account
+      </NavDashboard>
+    </div>
+  );
+}
